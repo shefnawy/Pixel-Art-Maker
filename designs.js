954bd401cdda880188975d469b82054388df7a77
Salma Hefnawy <swalid@aucegypt.edu>
2019-10-08 14:28:27 +0200

    Task

--- designs.js
+++ designs.js
@@ -1,9 +1,31 @@
 // Select color input
+let colorInput = document.getElementById("colorPicker");
 // Select size input
+let sizePicker = document.getElementById("sizePicker");
+let pixelCanvas = document.getElementById("pixelCanvas");
 
 // When size is submitted by the user, call makeGrid()
+makeGrid();
 
+//submit button
+sizePicker.onsubmit = function(e) {
+  e.preventDefault();
+  pixelCanvas.firstElementChild.remove();
+  makeGrid();
+};
+
+//make grid function
 function makeGrid() {
   // Your code goes here!
+  let height = document.getElementById("inputHeight").value;
+  let width = document.getElementById("inputWidth").value;
+  for (let i = 0; i < height; i++) {
+    let row = pixelCanvas.insertRow(i);
+    for (let j = 0; j < width; j++) {
+      let column = row.insertCell(j);
+      column.addEventListener("click", e => {
+        column.style.backgroundColor = colorInput.value;
+      });
+    }
+  }
 }
-
